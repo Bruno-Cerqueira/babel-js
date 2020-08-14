de5d9487eb9f935f1c75327b51ce3b1a4f89f278
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2020-08-14 04:42:01 -0300

    Create basic pokedex to show data from fetch.

--- src/index.js
+++ src/index.js
@@ -4,23 +4,34 @@ import { Pokemon } from './pokemon.js';
 
 const status = response => {
   if(response.status === 200) return Promise.resolve(response);
-  if(response.status !== 200) return Promise.reject(new Error(response.statusText));
+  if(response.status !== 200) return Promise.reject(response);
 }
 
 const json = response => response.json();
 
-fetch('https://pokeapi.co/api/v2/pokemon/1')
-  .then(status)
-  .then(json)
-  .then(data => {
-    const extractedData = [
-      "id",
-      "name",
-      "base_experience",
-      "height",
-      "weight",
-      "types"
-     ]
-    const pokeAttributes = Util.filterObj(data, extractedData);
-    console.log(new Pokemon(pokeAttributes))
-  })
+
+document.getElementById("searched-click").addEventListener("click", () => {
+  const pokeName = document.getElementById("searched-pokemon").value;
+  getPokemon(pokeName);
+})
+
+const getPokemon = name => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(status)
+    .then(json)
+    .then(data => {
+      const extractedData = [
+        "id",
+        "name",
+        "base_experience",
+        "height",
+        "weight",
+        "types"
+       ]
+      const pokeAttributes = Util.filterObj(data, extractedData);
+      console.log(new Pokemon(pokeAttributes))
+    })
+    .catch(_ => {
+      alert("Pokemon not found!")
+    })
+}
